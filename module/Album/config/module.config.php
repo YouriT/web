34057c0c2e8e9158676cfcfc460f659364e7a020
YouriT <ytolstoy@gmail.com>
2012-05-26 23:00:15 +0200

    Album added

---
+++ module/Album/config/module.config.php
@@ -0,0 +1,13 @@
+<?php
+return array(
+    'controller' => array(
+        'classes' => array(
+            'album/album' => 'Album\Controller\AlbumController',
+        ),
+    ),
+    'view_manager' => array(
+        'template_path_stack' => array(
+            __DIR__ . '/../view',
+        ),
+    ),
+);
